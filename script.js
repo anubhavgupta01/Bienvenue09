1955aa490663b6667ea4947921704f8e470a5777
Anubhav Gupta <anubhav0096.be20@chitkara.edu.in>
2022-10-17 11:51:30 +0530

    added files

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+//  <script>
+
+//                          <h1>My First JavaScript</h1>
+
+//                         <button type="button"
+//                             onclick="document.getElementById('demo').innerHTML = Date()">
+//                             Click me to display Date and Time.</button>
+
+//                         <p id="demo"></p>
+
+
+//     </script>
